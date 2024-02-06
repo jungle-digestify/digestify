405a85981065c7b7d99bb5288ab2d6b4744dbeb2
chohyunji1007 <10guswl7@gmail.com>
2024-02-06 22:34:57 +0900

    auth 우회 코드 추가

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,12 @@
 import { auth } from "@/auth";
-
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
 export const currentUser = async () => {
-  const session = await auth();
-  return session?.user;
+  // const session = await auth();
+  // return session?.user;
+  const [user] = await db.select().from(users).where(eq(users.email, process.env.USER_EMAIL!))
+  return user;
 };
 
 export const currentRole = async () => {
