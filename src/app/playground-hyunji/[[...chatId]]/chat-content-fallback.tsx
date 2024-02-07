b6f79883aa78744a88da9801836869bd9af7f2d7
sungjae bae <hsm0156@gmail.com>
2024-02-07 13:39:40 +0900

    feat: retry 추가 (streaming 안됨 ㅜㅜ)

---
+++ src/app/playground-hyunji/[[...chatId]]/chat-content-fallback.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useRef, useState } from "react";
+import { Button } from "../ui/button";
+
+export default function ChatContentFallback({ chatId }: { chatId: string }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const abortControllerRef = useRef<AbortController | null>(null);
+
+  const handleRetry = async () => {
+    setIsLoading(true);
+
+    let body = "";
+    body = JSON.stringify({ content: "test", chatId });
+
+    try {
+      abortControllerRef.current = new AbortController();
+      await fetch("/api/retry", {
+        method: "POST",
+        body: body,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        signal: abortControllerRef.current.signal,
+      });
+    } catch (error: any) {
+      if (error.name !== "AbortError") {
+        alert("Error sending message");
+      }
+    }
+
+    setIsLoading(false);
+  };
+
+  const handleClick = (e: MouseEvent) => {
+    console.log(e);
+  };
+
+  return (
+    <div className="h-full max-w-4xl w-full mx-auto flex-1 px-5 py-5 prose dark:prose-invert overflow-y-auto">
+      <p>open ai와의 통신이 실패했습니다 (1분에 최대 3번 질의 가능)</p>
+      <Button onClick={handleRetry} disabled={isLoading}>
+        재시도하기
+      </Button>
+    </div>
+  );
+}
