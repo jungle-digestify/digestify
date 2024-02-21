12f27c5e5459241ebef91684734a604d2cf8c4da
sungjae bae <hsm0156@gmail.com>
2024-02-21 13:45:21 +0900

    gin index추가

--- drizzle/0001_black_russian.sql
+++ drizzle/0001_black_russian.sql
@@ -1,1 +1,3 @@
 ALTER TABLE "messages" ADD COLUMN "vec" tsvector GENERATED ALWAYS AS (setweight(to_tsvector('config_2_gram_cjk', coalesce(content, '')), 'A')) STORED;
+
+CREATE index vec_gin ON messages USING gin(vec);
